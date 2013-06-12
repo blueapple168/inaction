4f6013c54d68bcb952f6f021a7056c469483f373
Wonder <wonderbeyond@gmai.com>
2013-06-12 22:56:35 +0800

    update setup.py: add inaction.py as script file.

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup
 
 setup(name='inaction',
-      version='1.0',
+      version='1.0.1',
       description='Run specified commands while specified files changes.',
       author='Wonder Beyond',
       author_email='wonderbeyond@gmail.com',
       url='https://github.com/wonderbeyond/inaction',
       py_modules=['inaction'],
+      scripts=['inaction.py'],
       install_requires=['pyinotify'],
      )
